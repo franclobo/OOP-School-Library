2c1b7df8c76f150b5beca362dc8e31232dcc00ef
franclobo <fjbl2788@gmail.com>
2022-08-10 18:42:18 -0500

    Add Person, Student and Teacher classes

---
+++ classes/student.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Student < Person
+
+  def initialize(name: 'Unknow', age, parent_permission: true, classroom)
+    super(name: name, age: age, parent_permission: parent_permission)
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
